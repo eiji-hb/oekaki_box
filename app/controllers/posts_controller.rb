ff8ef2ab76af3c0f6064f10e437cf83e07f629b1
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-14 22:11:42 +0900

    post側のbefore_action追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit,:update,:destroy]
   def new
     @post = Post.new
   end
@@ -36,11 +38,18 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to posts_path
   end
-end
 
 
-private
+  private
+  def post_params
+    params.require(:post).permit(:name,:description)
+  end
 
-def post_params
-  params.require(:post).permit(:name,:description)
+  def correct_user
+    @post = Post.find(params[:id])
+    unless current_user.id == @post.user_id
+      flash[:notice] = "権限がありません"
+      redirect_to root_path
+    end
+  end
 end
