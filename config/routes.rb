2a84ee58e2683ea5fdd6e1e5e53fc8fc3742e87d
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-15 11:03:19 +0900

    users_index_show追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   resources :posts
   devise_for :users
+  resources :users, only: [:show,:index]
   root 'pages#home'
   get 'pages/about' , to: 'pages#about'
 end
