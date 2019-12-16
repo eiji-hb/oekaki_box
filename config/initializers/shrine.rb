bd64513b39ff3325b3878b98ce92b7ec3e8766f7
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-16 23:57:29 +0900

    フォロー機能導入

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,11 +1,24 @@
 require "shrine"
 require "shrine/storage/file_system"
+require 'shrine/storage/s3'
+# if Rails.env.production?
+#     s3_options = {
+#         access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+#         secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+#         region:            Rails.application.credentials.dig(:aws, :region),
+#         bucket:            Rails.application.credentials.dig(:aws, :bucket),}
+#     Shrine.storages = {
+#         cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+#         store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
+# else
+    Shrine.storages = {
+        cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+        store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")}
 
-Shrine.storages = {
-    # temporary
-    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-    # permanent
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")}
+# end
+
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
+Shrine.plugin :data_uri
+Shrine.plugin :determine_mime_type
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_151811) do
+ActiveRecord::Schema.define(version: 2019_12_16_043551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2019_12_15_151811) do
     t.text "image_data"
     t.text "dataURI"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
