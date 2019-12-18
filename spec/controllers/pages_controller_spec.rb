fea636c79dda4157a6462cf7a9699b4c3455c75c
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-18 14:59:51 +0900

    user,page/controllerのrspec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe PagesController, type: :controller do
 
   describe "GET #home" do
-    it "returns http success" do
+    it "ホーム画面が表示される" do
       get :home
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET #about" do
-    it "returns http success" do
+    it "about画面が表示される" do
       get :about
       expect(response).to have_http_status(:success)
     end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-
-  describe "GET #index" do
-    it "returns http success" do
+  before do
+    @user = FactoryBot.create(:lily)
+  end
+  describe "#index" do
+    it "登録ユーザー表示" do
+      sign_in @user
       get :index
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe "#show" do
+    context "ログイン時" do
+      it "ユーザー詳細" do
+        sign_in @user
+        get :show, params: {id: @user.id}
+        expect(response).to have_http_status(:success)
+      end
+    end
+    context "非ログイン時" do
+      it "ログインページにリダイレクトされる" do
+        get :show, params: {id: @user.id}
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
