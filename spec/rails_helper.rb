bd8ddb51e2d11562d3b03fb8c5d5e4f569bc5267
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-18 21:18:02 +0900

    system_spec導入

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,16 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  config.before(:each) do |example|
+    if example.metadata[:type] == :system
+      if example.metadata[:js]
+        driven_by :selenium_chrome_headless, screen_size: [1400, 1400]
+      else
+        driven_by :rack_test
+      end
+    end
+  end
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
