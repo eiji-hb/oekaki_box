a8d199338bf2389d40c266cbba5d0b9a4ecbb3c8
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-19 16:40:48 +0900

    バリテーション修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   include ImageUploader[:image]
   belongs_to :user
   validates :name,{presence: true,length:{maximum: 30}}
-  validates :description,{presence: true,length:{maximum: 30}}
+  validates :description,{presence: true,length:{maximum: 130}}
 end
