8b92817e848fb36187fba42efa59b6fe4a3cff99
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-21 00:32:42 +0900

    like改修

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,7 @@ class LikesController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
   def create
     @like = current_user.likes.create(like_params)
-    # redirect_to post_path(@post)
-    # render 'like.js.erb'
+
     respond_to do |format|
       format.html { redirect_to @post }
       format.js
@@ -13,8 +12,6 @@ class LikesController < ApplicationController
   def destroy
     @like = Like.find_by(like_params, user_id: current_user.id)
     @like.destroy
-    # render 'like.js.erb'
-    # # redirect_to post_path(@post)
     respond_to do |format|
       format.html { redirect_to @post }
       format.js
