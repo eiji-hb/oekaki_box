4f122c77a36f4d23b5828afaed47d4db7b8b188b
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-20 10:25:38 +0900

    likedController導入

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,21 @@
+class LikesController < ApplicationController
+  before_action :set_post, only: [:create, :destroy]
+  def create
+    @like = current_user.likes.create(like_params)
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @like = Like.find_by(like_params, user_id: current_user.id)
+    @like.destroy
+    redirect_to post_path(@post)
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+  def like_params
+    params.permit(:post_id)
+  end
+end
