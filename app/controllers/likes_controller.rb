44362fd3a7faf325e991c2326b339b5766d116fe
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-21 00:11:15 +0900

    非同期通信導入

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,23 @@ class LikesController < ApplicationController
   before_action :set_post, only: [:create, :destroy]
   def create
     @like = current_user.likes.create(like_params)
-    redirect_to post_path(@post)
+    # redirect_to post_path(@post)
+    # render 'like.js.erb'
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 
   def destroy
     @like = Like.find_by(like_params, user_id: current_user.id)
     @like.destroy
-    redirect_to post_path(@post)
+    # render 'like.js.erb'
+    # # redirect_to post_path(@post)
+    respond_to do |format|
+      format.html { redirect_to @post }
+      format.js
+    end
   end
 
   private
