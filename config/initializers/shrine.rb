aefc35dd5ebbf5b31e8c0a186d69c983ace62914
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-25 11:21:55 +0900

    s3導入

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,21 +1,21 @@
 require "shrine"
 require "shrine/storage/file_system"
 require 'shrine/storage/s3'
-# if Rails.env.production?
-#     s3_options = {
-#         access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
-#         secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-#         region:            Rails.application.credentials.dig(:aws, :region),
-#         bucket:            Rails.application.credentials.dig(:aws, :bucket),}
-#     Shrine.storages = {
-#         cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-#         store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
-# else
+if Rails.env.production?
+    s3_options = {
+        access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+        secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
+        region:            Rails.application.credentials.dig(:aws, :region),
+        bucket:            Rails.application.credentials.dig(:aws, :bucket),}
+    Shrine.storages = {
+        cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+        store: Shrine::Storage::S3.new(prefix: 'store', **s3_options)}
+else
     Shrine.storages = {
         cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
         store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")}
 
-# end
+end
 
 
 Shrine.plugin :activerecord
