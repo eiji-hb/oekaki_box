136223451f6feb6f8647f09dc5e2e2740c101c82
eiji-hb <huuyaaar5@yahoo.co.jp>
2019-12-27 11:30:43 +0900

    認証キーを新たに作った

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -54,7 +54,7 @@ server "18.177.102.53",
   roles: %w{web app},
   ssh_options: {
     user: "ec2-user",
-    keys: %w(~/.ssh/aws-and-infra-ssh-key.pem),
+    keys: %w(~/.ssh/oekaki_box_rsa),
     forward_agent: true,
     # auth_methods: %w(publickey password)
     # password: "please use keys"
