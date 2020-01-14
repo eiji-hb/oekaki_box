02a7650621149ab550a7c04ce567f826dbeac970
eiji-hb <huuyaaar5@yahoo.co.jp>
2020-01-14 16:34:34 +0900

    relationship_rspec導入

---
+++ spec/controllers/relationships_controller_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe RelationshipsController, type: :controller do
+  before do
+    @user = FactoryBot.create(:lily)
+    @other_user = FactoryBot.create(:bob)
+    # @post = FactoryBot.create(:post, user: @user)
+  end
+  describe "#create" do
+    it "フォローする" do
+      sign_in @user
+      expect{
+        post :create,params: { followed_id: @other_user.id }, xhr: true
+      }.to change(Relationship, :count).by(1)
+    end
+  end
+
+  describe "#destroy" do
+    it "フォローを外す" do
+      sign_in @user
+      @relationship = FactoryBot.create(:relationship, followed_id: @other_user.id, follower_id: @user.id)
+
+      expect{
+        delete :destroy,params: {followed_id: @other_user.id,follower_id: @user.id,id:@relationship.id}, xhr: true
+      }.to change(Relationship, :count).by(-1)
+    end
+  end
+end
